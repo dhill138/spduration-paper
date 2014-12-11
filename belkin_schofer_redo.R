7214a14b5878599fd9f644b73798a301fe92cb5b
dhill138 <dwh138@gmail.com>
2014-12-11 15:05:07 -0500

    Update belkin_schofer_redo.R
    Added log-logistic and optional argument for covariate values to hazard plot.

--- belkin_schofer_redo.R
+++ belkin_schofer_redo.R
@@ -4,7 +4,7 @@
 user <- Sys.info()[["user"]]
 wd <- switch(user,
              andybega="~/Work/spduration-paper",
-             danielhill="/Users/danielhill/Documents/spduration-paper")
+             danielhill="/Users/danielhill/Documents/spduration")
 dropbox <- switch(user,
                   andybega="~/Dropbox/Work/gallium/data",
                   danielhill="")
@@ -12,11 +12,13 @@ setwd(wd)
 
 
 library(spduration)
+library(survival)
 library(foreign)
 library(xtable)
 library(separationplot)
 
-coup.data <- read.dta(file.path(dropbox, "BelkinSchoferTable4.dta"))
+#coup.data <- read.dta(file.path(dropbox, "BelkinSchoferTable4.dta"))
+coup.data <- read.dta(file="Belkin_Schofer_Data/BelkinSchoferTable4.dta")
 coup.data$coup<-as.numeric(coup.data$coup)-1
 
 ### replicate Column 4 of Table 4 to make sure the data are ok
@@ -37,6 +39,16 @@ summary(loglog.model<-spdur(duration~Military.Regime+Instability+Recent.War+Regi
 		Recent.War+Regional.Conflict+
 		South.Am.+Central.Am.,data=new.coup.data,distr="loglog"))
 xtable(loglog.model,caption="Coup model with log-logistic hazard")
+
+### compare with survreg. They're not really close. But survreg doesn't allow for time-varying covariates.
+survival.data<-new.coup.data[new.coup.data$end.spell==1&!is.na(new.coup.data$end.spell),]
+survival<-Surv(survival.data$duration,survival.data$failure)
+
+surv.weib.model<-survreg(survival~Military.Regime+Instability+Recent.War+Regional.Conflict,data=survival.data)
+weib.model$base
+
+surv.loglog.model<-survreg(survival~Military.Regime+Instability+Recent.War+Regional.Conflict,data=survival.data,dist="loglogistic")
+loglog.model$base
 
 ### use plot function to compare in-sample predictions 
 #pdf(file="in-sample.pdf",width=9,height=3)
@@ -66,7 +78,7 @@ weib.preds<-as.numeric(weib.model2$test.p)
 loglog.model2<-spdurCrisp(duration~Military.Regime+Instability+Recent.War+Regional.Conflict,
 		atrisk~Coup.Risk+GDP.cap.+Military.Regime+Recent.War+Regional.Conflict+
 		South.Am.,
-		last='end.spell',train=coup.train,test=coup.test,pred=coup.test,,stat='conditional risk',distr="loglog")
+		last='end.spell',train=coup.train,test=coup.test,pred=coup.test,stat='conditional risk',distr="loglog")
 loglog.preds<-as.numeric(loglog.model2$test.p)
 
 #pdf(file="out-of-sample.pdf",width=9,height=3)
@@ -78,34 +90,63 @@ separationplot(loglog.preds,coup.test$Coup,newplot=F,show.expected=T,lwd1=5,lwd2
 
 ### plot the hazard rate
 
-plot.hazard<-function(thing){
+plot.hazard<-function(thing,xvals='mean',zvals='mean'){
 	dur.dat<-thing$mf.dur
 	risk.dat<-thing$mf.risk 
 	ti<-seq(1, max(dur.dat[, 1])*1.2, length.out=100)
 	X<-cbind(1, dur.dat[ ,2:dim(dur.dat)[2]])
 	Z<-cbind(1, risk.dat[ ,2:dim(risk.dat)[2]])
 
-	beta<-weib.model$coef[1:ncol(X)]
-	gamma<-weib.model$coef[(ncol(X) + 1):(ncol(X) + ncol(Z))]
-	a<-weib.model$coef[ncol(X) + ncol(Z) + 1]
+	beta<-thing$coef[1:ncol(X)]
+	gamma<-thing$coef[(ncol(X) + 1):(ncol(X) + ncol(Z))]
+	a<-thing$coef[ncol(X) + ncol(Z) + 1]
 	alpha<-exp(-a)
 
-	mean.X<-apply(X,2,mean)
-	mean.Z<-apply(Z,2,mean)
-
-	lambda<-pmax(1e-10, exp(-mean.X %*% beta))
-	cure<-1 - plogis(mean.Z %*% gamma)
+	if (xvals=='mean'){
+		X.vals<-apply(X,2,mean)		
+		} else{
+		  X.vals<-c(1,xvals)			
+		}
+	
+	if (zvals=='mean'){
+		Z.vals<-apply(Z,2,mean)
+		} else{
+		  Z.vals<-c(1,zvals)
+		}
+		
+	lambda<-pmax(1e-10, exp(-X.vals %*% beta))
+	cure<-1 - plogis(Z.vals %*% gamma)
 
 	preds<-vector(length=length(ti))
-	for(i in 1:length(ti)){
-		st<-exp(-(lambda * ti[i])^alpha)
-		cure.t<-cure / pmax(1e-10, (st + cure * (1 - st)))
-		atrisk.t<-1 - cure.t
-		ft<-lambda * alpha * (lambda * ti[i])^(alpha-1) * exp(-(lambda * ti[i])^alpha)
-		preds[i]<-atrisk.t * ft / pmax(1e-10, (cure.t + atrisk.t * st))
+
+	if (thing$distr=="weibull"){
+		for(i in 1:length(ti)){
+			st<-exp(-(lambda * ti[i])^alpha)
+			cure.t<-cure / pmax(1e-10, (st + cure * (1 - st)))
+			atrisk.t<-1 - cure.t
+			ft<-lambda * alpha * (lambda * ti[i])^(alpha-1) * exp(-(lambda * ti[i])^alpha)
+			preds[i]<-atrisk.t * ft / pmax(1e-10, (cure.t + atrisk.t * st))
+		}
 	}
+	
+	if (thing$distr=="loglog"){
+		for(i in 1:length(ti)){
+			st<-1/(1+(lambda * ti[i])^alpha)
+			cure.t<-cure / pmax(1e-10, (st + cure * (1 - st)))
+			atrisk.t<-1 - cure.t
+			ft<-(lambda * alpha * (lambda * ti[i])^(alpha-1)) / ((1 + (lambda * ti[i])^alpha)^2)
+			preds[i]<-atrisk.t * ft / pmax(1e-10, (cure.t + atrisk.t * st))
+		}
+	}
+	
 plot(ti,preds,type="l",xlab="Time",ylab="Conditional Hazard")
+
 }
+
+plot.hazard(weib.model)
+plot.hazard(loglog.model)
+plot.hazard(weib.model,xvals=c(1, 2, 1, 0),zvals=c(1,7,1,1,0,0,1))
+plot.hazard(loglog.model,xvals=c(1, 2, 1, 0),zvals=c(1,7,1,1,0,0,1))
 
 # playing around
 shape <- exp(weib.model$coefficients["log(alpha)"])
