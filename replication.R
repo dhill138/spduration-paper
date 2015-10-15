fa78d386a1a81eab581f6c8b1505308a9ce8a064
Andreas Beger <adbeger@gmail.com>
2015-10-15 17:01:01 +0300

    started working on replication script

---
+++ replication.R
@@ -0,0 +1,9 @@
+#  Replication script for JSS manuscript examples
+
+
+library(spduration)
+#data(bscoup)
+bscoup <- read.dta("data/BelkinSchoferTable4.dta")
+bscoup$coup <- ifelse(bscoup$coup=="yes", 1, 0)
+bscoup <- add_duration(bscoup, "coup", unitID="countryid", tID="year",
+                       freq="year")
